fb9afd9f47b671e29bba2e078f218ff9c7b0ac81
Enidbii <chebsenid@gmail.com>
2023-01-12 10:35:12 -0800

    modules operations

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ from sys import argv
 if __name__ = "__main__":
     i = 1
     args = len(argv) - 1
-    print(f"{args} {'arguments' if args == 1 else 'arguments'}", end="")
+    print(f"{args} {'argument' if args == 1 else 'arguments'}", end="")
     print(f"{'.' if args == 0 else ':'}")
     while (i <= args):
         print(f"{i}: {argv[i]}")
