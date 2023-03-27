6b574cd811be4196ec47d52de59aac29e6d83ae7
Enidbii <chebsenid@gmail.com>
2023-03-27 08:53:21 -0700

    exceptions

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,7 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list = [], x = 0):
-    for x in my_list:
+    rand_no = 0
+    for i in range(x):
         try:
-            print(my_list[x])
+            print(my_list[i], end="")
+            rand_no += 1
         except IndexError:
             print("Sorry, that index does not exist")
+            break
+    print()
+    return rand_no
