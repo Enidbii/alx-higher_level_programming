b27e37950e47ad10ebd19ff34c657c0af6fdfea4
Enidbii <chebsenid@gmail.com>
2023-03-28 13:11:19 -0700

    squares

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+""" Square module """
+
+class Square:
+
+    """ Initializing the class """
+
+   # def __init__(self, size):
+    #    __size = size
+
+
+    """ Create an instance that defines size """
+    __size
