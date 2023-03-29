f42b75fa2a2dcfcfcec13c0eb164203057c4e9d2
Enidbii <chebsenid@gmail.com>
2023-03-28 23:56:45 -0700

    squares

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -8,18 +8,10 @@ class Square:
     
     def __init(self, size = 0):
         self.__size = size
-
-"""
-call the class using try
-and exception
-
-args: size of the square
-
-"""
-try:
-    Square()
-except TypeError:
-    raise size must be an integer
-except ValueError:
-    if size < 0:
-        raise size must be >= 0
+        try:
+            Square()
+        except TypeError:
+            raise size must be an integer
+        except ValueError:
+            if size < 0:
+                raise size must be >= 0
