8e93175b56a402c8c9216ef3f47ce9962fd08ec5
Enidbii <chebsenid@gmail.com>
2023-03-29 05:40:01 -0700

    area

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -11,12 +11,7 @@ class Square:
         Args:
             size: size of square
         """
-        if type(size) is not int:
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
-        else:
-            self.__size = size
+        self.size(__size)
 
     def area(self):
         """
@@ -27,3 +22,27 @@ class Square:
         """
         area = self.__size * self.__size
         return area
+    
+    @property
+    def size(self):
+        """
+        Defines private instance size
+
+        Args:
+            size: size of the square
+        """
+        return self.__size
+
+    def size(self, value):
+        """
+        sets the size of the square
+
+        Args:
+            value: size of the square
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
