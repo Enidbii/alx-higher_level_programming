2ed6edc6d96d3591175bc35c0075332db2045759
Enidbii <chebsenid@gmail.com>
2023-03-29 01:36:25 -0700

    squares

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Squrae module """
+
+
+class Square:
+    """ declares a square class """
+    def __init__(self, size=0) -> None:
+        """ Initializes class attrributes
+
+        Args:
+            size: size of square
+        """
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ declares and defines area attributes
+
+        Args:
+            area: area itself
+        """
+        area = self.size * self.size
+        return area
