b0cc15652e61d1e64d418e3454e34ca731d3ef35
Enidbii <chebsenid@gmail.com>
2023-04-11 12:50:17 -0700

    squares

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""square module """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ inherits from class rectangle """
+
+    def __init__(self, size):
+        """ initialise square class
+        Args:
+            size: size of square
+        """
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ returns area of square """
+        return self.__size ** 2
