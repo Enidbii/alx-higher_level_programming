3a6e72e9b85245b027e9f7de19b1d548fa9c70ef
Enidbii <chebsenid@gmail.com>
2023-04-11 04:46:57 -0700

    list

---
+++ 0x0A-python-inheritance/tests/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" My list module """
+
+
+class MyList(list):
+    """ MyList inherits from list """
+
+    def print_sorted(self):
+        """ prints list in a sorted way """
+        return sort(list())
