54b27705c44e6f61a32cd3602ef18df8ac7590b8
Enidbii <chebsenid@gmail.com>
2023-04-11 11:47:39 -0700

    rec

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3r
+""" Base Geometry module """
+
+
+class BaseGeometry:
+    """ Base Geometry class """
+    def __init__(self):
+        pass
+
+    def area(self):
+        """ solve area """
+        raise Exception("area not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        validates value
+         Args:
+            name: of integer
+            value: value of validated
+        """
+        if type(value) is not int:
+            raise TypeError("{:s} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{:s} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle class """
+    def __init__(self, width, height):
+        """
+        Initializing function
+        Args:
+            width: width of rectangle
+            height: height of rectangle
+        """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ area of rectangle """
+        return self__width * self__height
+
+    def __str__(self):
+        """ informal string representation of rectangle """
+        return "[Rectangle] {:d}/{:d}".format(self.width, self.height)
