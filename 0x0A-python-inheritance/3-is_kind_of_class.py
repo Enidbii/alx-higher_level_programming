a9173f0807ebde71ebb58051e043d3427fc8b586
Enidbii <chebsenid@gmail.com>
2023-04-11 08:51:31 -0700

    lookup

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" class inheritance """
+
+
+def is_kind_of_class(obj, a_class):
+    """ 
+        returns true if instance of class and false
+        if otherwise
+    """
+
