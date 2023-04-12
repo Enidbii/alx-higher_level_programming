260c7ad7e8c2044113a96ac73a972025153e18a5
Enidbii <chebsenid@gmail.com>
2023-04-12 03:21:42 -0700

    read file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,6 @@
 
 def write_file(filename="", text=""):
     """ writes string to a txt file and returns writen chars """
-    with open(filename, 'r+', encoding="utf-8") as text_file:
+    with open(filename, "w", encoding="utf-8") as text_file:
         write_text = text_file.write(text)
         return (write_text)
