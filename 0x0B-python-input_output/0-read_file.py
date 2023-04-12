9c961ed053c1c28055db3a6b3ce3218ddd29b338
Enidbii <chebsenid@gmail.com>
2023-04-12 02:46:23 -0700

    readfile

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,4 +6,4 @@ def read_file(filename=""):
     """ reads file and prints it out to stdout """
     with open("filename", 'r', encoding="utf-8") as f:
         read_data = f.read(size)
-        print(f)
+        print(read_data)
