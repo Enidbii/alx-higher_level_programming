d5eaf03b005fca6944e2fe1acc9d2321f4001928
Enidbii <chebsenid@gmail.com>
2023-04-12 03:07:13 -0700

    read file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -7,5 +7,4 @@ def read_file(filename=""):
     filename = "UTF8"
     with open("filename", 'r', encoding="utf-8") as f:
         read_data = f.read()
-
-    print(read_data)
+        print(read_data, end="")
