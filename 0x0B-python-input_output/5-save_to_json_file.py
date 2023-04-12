6806c8d8f4897f4f1f4f2c3d933ef51de88a1a35
Enidbii <chebsenid@gmail.com>
2023-04-12 03:59:50 -0700

    jsonfile

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -10,4 +10,4 @@ def save_to_json_file(my_obj, filename):
     x = json.dumps(my_obj)
     filename = ""
     with open(filename, "w", encoding="utf-8") as file_txt:
-        written = file_txt.write(x)
+        file_txt.write(x)
