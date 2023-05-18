20399a5cdd26f939c20d02e2b85e4f9c58231057
EnidBii <chebsenid@gmail.com>
2023-05-18 03:44:27 -0700

    sql

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,4 +1,4 @@
 -- lists all records of the table second_table of the database hbtn_0c_0 in your MySQL server
---SELECT .. FROM .. WHERE .. ORDER BY
+-- SELECT .. FROM .. WHERE .. ORDER BY
 
 SELECT score, name FROM second_table WHERE name IS NOT NULL AND name != '' ORDER BY score DESC;
