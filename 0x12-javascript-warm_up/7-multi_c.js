81977eca9ec0700677b23db9d58de055a7265de8
EnidBii <chebsenid@gmail.com>
2023-06-14 05:05:46 -0700

    correct

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
-	console.log('Missing number of occurrences');
+  console.log('Missing number of occurrences');
 } else {
-	const x = parseInt(process.argv[2]);
-	let y = 0;
-	while (y < x) {
-		console.log('C is fun');
-		y++;
-	}
+  const x = parseInt(process.argv[2]);
+  let y = 0;
+  while (y < x) {
+    console.log('C is fun');
+    y++;
+  }
 }
