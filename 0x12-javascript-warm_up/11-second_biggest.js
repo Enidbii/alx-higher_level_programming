3ab6245ba0857a758153539f2a6896d9c97aab97
EnidBii <chebsenid@gmail.com>
2023-06-14 04:49:20 -0700

    correct

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,9 +3,6 @@ if (process.argv.length <= 3) {
   console.log('0');
 } else {
   const num1 = process.argv.slice(2).map(Number);
-  const num2 = num1.sort(
-	  function (y, z) {
-		  return z - y;
-	  })[1];
+  const num2 = num1.sort(function (y, z) { return z - y; })[1];
   console.log(num2);
 }
