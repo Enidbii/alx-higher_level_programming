dd0edea93bc93f90294930ba9097c83cc213b02d
EnidBii <chebsenid@gmail.com>
2023-06-19 03:08:08 -0700

    model

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -5,7 +5,6 @@ Base = declarative_base()
 """
 import sqlalchemy
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Sequence
 from sqlalchemy import Column, Integer, String
 
 Base = declarative_base()
@@ -14,5 +13,5 @@ Base = declarative_base()
 class State(Base):
     __tablename__ = 'states'
 
-    id = Column(Integer, Sequence('states_id'), primary_key=True)
+    id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
