fe33aaeb90a4ec1b0b9154cbfd0812bce23fb170
Enidbii <chebsenid@gmail.com>
2023-07-04 21:42:17 +0300

    networks+py

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" takes your GitHub credentials (username and password)
+and uses the GitHub API to display your id """
+
+
+f __name__ == '__main__':
+    import sys
+    import requests
+
+    url = "https://api.github.com/user"
+    resp = requests.get(
+        url, auth=requests.auth.HTTPBasicAuth(sys.argv[1], sys.argv[2]))
+    respon = response.json()
+    print(respon.get('id'))
---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+""" akes in a URL, sends a request to the URL and
+displays the body of the response (decoded in utf-8). """
+
+
+if __name__ == "__main__":
+    import sys
+    import urllib.request
+    import urllib.error
+
+    url = urllib.request.Request(sys.argv[1])
+    try:
+        with urllib.request.urlopen(url) as response:
+            url_respond = response.read()
+            print(url_response.decode('utf-8'))
+
+    except urllib.error.HTTPError as p_error:
+        print("Error code: {}".format(p_error.code))
