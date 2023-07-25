0ab11cb7a71a0094eca08be08a28fbad8293e1b8
Enidbii <chebsenid@gmail.com>
2023-07-25 23:19:50 +0300

    js

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
 /* Write a str to a file */
 
-const file_path = require('file_path');
+const fs = require('fs');
 const filename = process.argv[2];
-const text_str = process.argv[3];
-fs.writeFile(filename, text_str, 'utf8', (err) => {
+const text = process.argv[3];
+fs.writeFile(filename, text, 'utf8', (err) => {
   if (err) console.log(err);
 });
