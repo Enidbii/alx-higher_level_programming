d452584fc419857e8b35e80ce15f10fc1c6f8ac6
Enidbii <chebsenid@gmail.com>
2023-07-26 01:18:02 +0300

    webscrape

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 /* gets content of a webpage and stores it in a file */
 
-const fle = require('fle');
-const req = require('req');
-req(process.argv[2]).pipe(fle.createWriteStream(process.argv[3]));
+const fs = require('fs');
+const request = require('request');
+request(process.argv[2]).pipe(fs.createWriteStream(process.argv[3]));
