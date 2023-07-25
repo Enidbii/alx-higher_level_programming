79ed5bdb58ddc69dede71094cdcec1f90b0efb1a
Enidbii <chebsenid@gmail.com>
2023-07-26 01:24:23 +0300

    webscrape

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 /* prints number of movies where character wedege antilles is */
 
-const req = require('request')
-req(process.argv[2], function (error, response, body) {
+const request = require('request');
+request(process.argv[2], function (error, response, body) {
   if (!error) {
     const reslt = JSON.parse(body).reslt;
     console.log(reslt.reduce((count, movie) => {
