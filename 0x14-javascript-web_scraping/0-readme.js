7f89f85dcaeb27f945cce9d04835855e06b7f7c0
Enidbii <chebsenid@gmail.com>
2023-07-25 23:16:55 +0300

    js

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* reads and prints the content of a file */
 
-const file = require('file');
+const fs = require('fs');
 const filename = process.argv[2];
 fs.readFile(filename, 'utf8', (err, data) => {
   if (err) console.log(err);
