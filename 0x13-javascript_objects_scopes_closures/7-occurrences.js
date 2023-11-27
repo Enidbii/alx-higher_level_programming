c56a5d7be30fa3644953b73404f374eaf87688f7
Enidbii <52915074+Enidbii@users.noreply.github.com>
2023-11-27 10:58:58 +0300

    Delete 0x13-javascript_objects_scopes_closures directory

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-
-exports.nbOccurences = function (list, searchElement) {
-  let count = 0;
-  list.forEach((occur) => {
-    if (occur === searchElement) count++;
-  });
-  return count;
-};
--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/node
-
-exports.esrever = function (list) {
-  return list.map((_, index, array) => array[array.length - 1 - index]);
-};
