ee6997d9c2fead171ed629a23fd8759e1f5670b8
Enidbii <52915074+Enidbii@users.noreply.github.com>
2023-11-27 10:59:15 +0300

    Delete 0x14-javascript-web_scraping directory

--- 0x14-javascript-web_scraping/0-readme.js
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/node
-/* reads and prints the content of a file */
-
-const fs = require('fs');
-const filename = process.argv[2];
-fs.readFile(filename, 'utf8', (err, data) => {
-  if (err) console.log(err);
-  else console.log(data.toString());
-});
--- 0x14-javascript-web_scraping/2-statuscode.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-/* Script that display status code of a GET request */
-
-const req = require('req');
-req.get(process.argv[2]).on('response', function (response) {
-  console.log(`code: ${response.statusCode}`);
-});
