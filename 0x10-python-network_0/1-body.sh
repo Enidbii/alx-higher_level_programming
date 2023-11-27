754d6e04517db5e3a6cdd0708c639a793c906dba
Enidbii <52915074+Enidbii@users.noreply.github.com>
2023-11-27 10:55:54 +0300

    Delete 0x10-python-network_0 directory

--- 0x10-python-network_0/1-body.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# takes URL, sends a GET request to URL, and display only body of a 200 status code response
-curl -s "$1" -X GET -L
--- 0x10-python-network_0/5-post_params.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# takes `URL, sends a POST request to the passed URL, and displays the body
-curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"
