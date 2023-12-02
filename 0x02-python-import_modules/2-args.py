c400731c23fa246f33006e8121fa4bea5bdcb2cd
Enidbii <chebsenid@gmail.com>
2023-12-02 11:32:47 +0300

    arghh

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,7 +2,7 @@
 if __name__ == "__main__":
     import sys
 
-    arguc = len(argv) - 1
+    arguc = len(sys.argv) - 1
     if arguc == 0:
         print("0 arguments.")
     elif arguc == 1:
@@ -10,4 +10,4 @@ if __name__ == "__main__":
     else:
         print("{} arguments:".format(arguc))
     for i in range(arguc):
-        print("{}: {}".format(i + 1, sys.argv[i + 1]]))
+        print("{}: {}".format(i + 1, sys.argv[i + 1]))
