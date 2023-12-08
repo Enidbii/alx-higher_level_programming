119d168cb6598055407d9a8c6036e978d8b99bd9
Enidbii <chebsenid@gmail.com>
2023-12-08 13:43:27 +0300

    listin

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    len_sen = len(sentence)
-    if (len_sen == 0):
-        new_tuple = (len_sen, None)
-    else:
-        new_tuple = (len_sen, sentence[0])
-
-    return(new_tuple)
+    if len(sentence) == 0:
+        return len(sentence), None
+    for i in sentence:
+        return (len(sentence), i)
